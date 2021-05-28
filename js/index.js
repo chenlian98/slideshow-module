1c7d88492633c9bae94954f4e663eed96a37ecd4
porridge <55335568+chenlian98@users.noreply.github.com>
2021-05-28 20:16:48 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -59,31 +59,3 @@ carousel('#banner7',{
 
 
 
-
-
-
-/*新歌首发tab列表切换*/
-function new_song_tab() {
-let nav = document.querySelectorAll('.new-song .new-song-list-left .list-text3');
-    //列表
-let list = document.querySelectorAll('.new-song .new-song-list-left .warp-new-song-list');
-// console.log(list)
-    for (let i = 0; i < nav.length; i++) {
-        nav[0].classList.add('active')//第一个显示高亮
-        list[0].classList.add('active')
-        //onmouseover
-        nav[i].onclick = function () {
-            //先删除所有的类名
-            for (let i = 0; i < nav.length; i++) {
-                nav[i].classList.remove('active')
-                list[i].classList.remove('active')
-            }
-            this.classList.add('active')
-            list[i].classList.add('active')
-        }
-
-    }
-
-
-}new_song_tab()
-
